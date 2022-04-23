987fe291b69da9f470ead34ca6f7be3e4c918b48
Lovely Sharma <lovelysingeras297@gmail.com>
2022-04-23 10:09:23 +0000

    good reviews matching completed

--- .ccls-cache/@home@runner@Assignment1/main.cpp
+++ .ccls-cache/@home@runner@Assignment1/main.cpp
@@ -1,17 +1,40 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <regex>
+
 using namespace std;
+
+
+
 int main() {
-  
+
+  //create file object
   fstream file;
+  string goodWords[]={"good","best","nice","excelellnt","great","satis","acceptable","fab","awesome","amazing"};
+  int goodlength=sizeof(goodWords)/sizeof(goodWords[0]);
+  string badWords[]={"bad","worst","poor","regret","awful","terrible","disgusting"};
+  int badlength=sizeof(badWords)/sizeof(badWords[0]);
+  //open file 
   file.open("file.txt",ios::in);
   if(!file){
     cout<<"file error";
   }else{
-    string tp;
-      while(getline(file, tp)){ 
-         cout << tp << "\n";       }
+    string review;
+    int counter=0;
+    //file traversal
+      while(getline(file, review)){
+        //array traversal
+        transform(review.begin(), review.end(), review.begin(), ::tolower);
+
+        for(int i=0;i<goodlength;i++){
+          regex reg(goodWords[i]);
+          regex reg1("not "+goodWords[i]);
+          if(regex_search(review,reg) && !regex_search(review,reg1)){
+            cout<<review<<"\n";
+          }
+        }
+      }
   }
   file.close();
   return 0;
